01bac2fc0fdf9f96d040d8d9d35443111a44883e
Jonathan Belcher <belcherj@gmail.com>
2018-02-07 16:57:41 -0500

    Update css to 2018

--- page-templates/libertyjs-2018-homepage.php
+++ page-templates/libertyjs-2018-homepage.php
@@ -6,47 +6,47 @@ Template Name: Homepage
 <?php get_header(); ?>
 
 
-<!-- <div class="ljs2017-header-cta__container">
-	<div class="ljs2017-header-cta__countdown-container">
+<!-- <div class="ljs2018-header-cta__container">
+	<div class="ljs2018-header-cta__countdown-container">
 	</div>
-	<div class="ljs2017-header-cta__cta-container">
+	<div class="ljs2018-header-cta__cta-container">
 		<p>
 			<strong>It was fun, see you next year!</strong>
 		</p>
 	</div>
 </div> -->
 
-<div class="ljs2017-header__container-overlay">
-	<div class="ljs2017-header__container">
-		<div class="ljs2017-header__info-container">
+<div class="ljs2018-header__container-overlay">
+	<div class="ljs2018-header__container">
+		<div class="ljs2018-header__info-container">
 			<img
-				class="ljs2017-header__info-logo"
-				src="<?php echo get_stylesheet_directory_uri(); ?>/img/ljs2017-primary-logo-white-yellow.png"
+				class="ljs2018-header__info-logo"
+				src="<?php echo get_stylesheet_directory_uri(); ?>/img/ljs2018-primary-logo-white-yellow.png"
 				alt="LibertyJS Logo">
-			<div class="ljs2017-header__info-location">
+			<div class="ljs2018-header__info-location">
 				OCTOBER 2018 - PHILADELPHIA, USA
 			</div>
 		</div>
 	</div>
 </div>
-<div class="ljs2017-summary__container" id="about">
-	<div class="ljs2017-summary__container-top">
-		<div class="ljs2017-summary__blurb">
-			<div class="ljs2017-summary__blurb-quote">
+<div class="ljs2018-summary__container" id="about">
+	<div class="ljs2018-summary__container-top">
+		<div class="ljs2018-summary__blurb">
+			<div class="ljs2018-summary__blurb-quote">
 				This is the greatest conference about JS in the world
 			</div>
-			<div class="ljs2017-summary__blurb-name">
+			<div class="ljs2018-summary__blurb-name">
 				Jonathan Belcher, JavaScript Electro Wizard
 			</div>
-			<div class="ljs2017-summary__blurb-location">
+			<div class="ljs2018-summary__blurb-location">
 				LibertyJS Organizer
 			</div>
 		</div>
-		<div class="ljs2017-summary__headline">
+		<div class="ljs2018-summary__headline">
 			<h1>TWO DAYS<br> GREAT WORKSHOPS<br> AWESOME SPEAKERS</h1>
 		</div>
 	</div>
-	<div class="ljs2017-summary__summary">
+	<div class="ljs2018-summary__summary">
 		<p>
 			<strong>The community-super-powered JavaScript tech conference</strong><br>
 			<p>
@@ -67,30 +67,30 @@ Template Name: Homepage
 		</p>
 	</div>
 </div>
-<div class="ljs2017-lastyear__container" id="2016">
-	<div class="ljs2017-lastyear__heading">
+<div class="ljs2018-lastyear__container" id="2016">
+	<div class="ljs2018-lastyear__heading">
 		<h2>#2016</h2>
-		<div class="ljs2017-lastyear__images">
+		<div class="ljs2018-lastyear__images">
 			<img
-				class="ljs2017-lastyear__image-large"
-				src="<?php echo get_stylesheet_directory_uri(); ?>/img/ljs2017-lastyear-1.jpg"
+				class="ljs2018-lastyear__image-large"
+				src="<?php echo get_stylesheet_directory_uri(); ?>/img/ljs2018-lastyear-1.jpg"
 				alt="LibertyJS 2016">
 			<img
-				class="ljs2017-lastyear__image-small"
-				src="<?php echo get_stylesheet_directory_uri(); ?>/img/ljs2017-lastyear-2.jpg"
+				class="ljs2018-lastyear__image-small"
+				src="<?php echo get_stylesheet_directory_uri(); ?>/img/ljs2018-lastyear-2.jpg"
 				alt="LibertyJS 2016">
 			<img
-				class="ljs2017-lastyear__image-small"
-				src="<?php echo get_stylesheet_directory_uri(); ?>/img/ljs2017-lastyear-3.jpg"
+				class="ljs2018-lastyear__image-small"
+				src="<?php echo get_stylesheet_directory_uri(); ?>/img/ljs2018-lastyear-3.jpg"
 				alt="LibertyJS 2016">
 			<img
-				class="ljs2017-lastyear__image-small"
-				src="<?php echo get_stylesheet_directory_uri(); ?>/img/ljs2017-lastyear-4.jpg"
+				class="ljs2018-lastyear__image-small"
+				src="<?php echo get_stylesheet_directory_uri(); ?>/img/ljs2018-lastyear-4.jpg"
 				alt="LibertyJS 2016">
 		</div>
-		<div class="ljs2017-lastyear__watch-container">
+		<div class="ljs2018-lastyear__watch-container">
 			<a href="https://www.youtube.com/channel/UCbbeyEpxDT53afqFUqG-KJQ" target="_blank">
-				<div class="ljs2017-lastyear__watch-play"></div>
+				<div class="ljs2018-lastyear__watch-play"></div>
 			</a>
 			<h3>
 				Missed out on 2016<br>
@@ -111,31 +111,31 @@ Template Name: Homepage
 		</div>
 	</div>
 </div>
-<div class="ljs2017-faq__container">
-	<div class="ljs2017-faq__questions">
-		<div class="ljs2017-faq__questions-column">
-			<div class="ljs2017-faq__question">
+<div class="ljs2018-faq__container">
+	<div class="ljs2018-faq__questions">
+		<div class="ljs2018-faq__questions-column">
+			<div class="ljs2018-faq__question">
 				<h3>Who should come to LibertyJS?</h3>
 				<p>
 					Anyone who is interested in JavaScript. If you want to learn, play
 					with, or work with JS
 				</p>
 			</div>
-			<div class="ljs2017-faq__question">
+			<div class="ljs2018-faq__question">
 				<h3>What's included with my ticket?</h3>
 				<p>
 					Two days of really amazing talks and workshops. Lunch is also provided!
 				</p>
 			</div>
-			<div class="ljs2017-faq__question">
+			<div class="ljs2018-faq__question">
 				<h3>How can we sponsor LibertyJS?</h3>
 				<p>
 					You can sponsor LibertyJS by going <a href="/sponsor">here</a>
 				</p>
 			</div>
 		</div>
-		<div class="ljs2017-faq__questions-column">
-			<div class="ljs2017-faq__question">
+		<div class="ljs2018-faq__questions-column">
+			<div class="ljs2018-faq__question">
 				<h3>Does LibertyJS have a Code of Conduct?</h3>
 				<p>
 					Indeed we do. You can read the Code of Conduct in full on
@@ -143,13 +143,13 @@ Template Name: Homepage
 					<a href="/code-of-conduct/">page.</a>
 				</p>
 			</div>
-			<div class="ljs2017-faq__question">
+			<div class="ljs2018-faq__question">
 				<h3>Is this the best JavaScript conference in Philadelphia?</h3>
 				<p>
 					Yes!
 				</p>
 			</div>
-			<div class="ljs2017-faq__question">
+			<div class="ljs2018-faq__question">
 				<h3>How can I contact the organizers of LibertyJS?</h3>
 				<p>
 					<a href="mailto:libertyjsconf@gmail.com">Get in touch!</a>
@@ -157,14 +157,14 @@ Template Name: Homepage
 			</div>
 		</div>
 	</div>
-	<div class="ljs2017-faq__blurb-container">
-		<div class="ljs2017-faq__blurb" id="faqs">
+	<div class="ljs2018-faq__blurb-container">
+		<div class="ljs2018-faq__blurb" id="faqs">
 			<h2>FAQ's</h2>
 		</div>
 	</div>
 </div>
-<div class="ljs2017-social__container">
-	<div class="ljs2017-social__facebook">
+<div class="ljs2018-social__container">
+	<div class="ljs2018-social__facebook">
 		<a
 			href="https://www.facebook.com/hashtag/libertyjs"
 			target="_blank"
@@ -172,7 +172,7 @@ Template Name: Homepage
 			<i class="fa fa-facebook-official" aria-hidden="true"></i>
 		</a>
 	</div>
-	<div class="ljs2017-social__twitter">
+	<div class="ljs2018-social__twitter">
 		<a
 			href="https://twitter.com/liberty_js"
 			target="_blank"
@@ -180,7 +180,7 @@ Template Name: Homepage
 			<i class="fa fa-twitter" aria-hidden="true"></i>
 		</a>
 	</div>
-	<div class="ljs2017-social__instagram">
+	<div class="ljs2018-social__instagram">
 		<a
 			href="https://www.instagram.com/explore/tags/libertyjs/"
 			target="_blank"
