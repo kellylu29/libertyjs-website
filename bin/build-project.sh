3f3a8ace0e60df0f29f37405921bf05893af4773
Jonathan Belcher <jonathan.belcher@automattic.com>
2018-09-05 15:14:51 -0400

    Update build-project.sh

--- bin/build-project.sh
+++ bin/build-project.sh
@@ -8,12 +8,6 @@ ORIGIN_URL=`git config --get remote.origin.url`
 
 echo "Started deploying"
 
-# Checkout deploy branch.
-if [ 'git branch | grep build-production' ]
-then
-  git branch -D build-production
-fi
-git checkout -b build-production
 
 
 
