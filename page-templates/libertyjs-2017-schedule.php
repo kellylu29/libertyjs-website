271cf7250d483d1d5b6c576c021b2d3a9762d073
Jonathan Belcher <belcherj@gmail.com>
2018-10-18 15:32:33 -0400

    Add 2018 schedule

--- page-templates/libertyjs-2017-schedule.php
+++ page-templates/libertyjs-2017-schedule.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Schedule
+Template Name: Schedule 2017
 */
 get_header();
 ?>
---
+++ page-templates/libertyjs-2018-schedule.php
@@ -0,0 +1,331 @@
+<?php
+/*
+Template Name: Schedule 2018
+*/
+get_header();
+?>
+
+<h1 class="ljs-template-info__heading">
+	<?php echo esc_html( get_the_title() ); ?>
+</h1>
+
+<div class="ljs-template-schedule__container" id="ljs-template-schedule__day1">
+	<div class="ljs-template-schedule__container-day">
+		<div class="ljs-template-schedule__date">
+			<p>NOV 1ST</p>
+			<p><span class="ljs-template-schedule__date-active">DAY 1</span> / <a href="#ljs-template-schedule__day2">DAY 2</a></p>
+		</div>
+		<div class="ljs-template-schedule__speakers">
+			<div class="ljs-template-schedule__divider">
+				<div>Registration 08:30 am</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-three">
+				<div class="ljs-template-schedule__speaker-body">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">08:30 - 09:30</span>
+						<span class="ljs-template-schedule__name">Breakfast and Registration</span>
+						<span class="ljs-template-schedule__title"></span>
+					</div>
+				</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-three">
+				<div class="ljs-template-schedule__speaker-body">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">09:30 - 09:40</span>
+						<span class="ljs-template-schedule__name">Opening Remarks</span>
+						<span class="ljs-template-schedule__title"></span>
+					</div>
+				</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-one">
+				<div class="ljs-template-schedule__speaker-body">
+					<img
+						src="https://hvz5h5ar027h1qc4-zippykid.netdna-ssl.com/wp-content/uploads/2018/10/IvanaVeliskova.jpg"
+						alt="Ivana Veliskova and Ryan Harris">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">9:50 - 11:25</span>
+						<span class="ljs-template-schedule__name"><a href="https://libertyjs.com/speakers/ivana-veliskova/">Ivana Veliskova</a> + <a href="https://libertyjs.com/speakers/ryan-harris/">Ryan Harris</a></span>
+						<span class="ljs-template-schedule__title">Build a Blog with the Great (JayS) Gatsby!</span>
+					</div>
+				</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-two">
+				<div class="ljs-template-schedule__speaker-body">
+					<img
+						src="https://hvz5h5ar027h1qc4-zippykid.netdna-ssl.com/wp-content/uploads/2018/10/juanlopez.jpeg"
+						alt="Juan Lopez">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">09:50 - 10:30</span>
+						<span class="ljs-template-schedule__name"><a href="https://libertyjs.com/speakers/juan-lopez/">Juan Lopez</a></span>
+						<span class="ljs-template-schedule__title">War stories of code standards and other monsters</span>
+					</div>
+				</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-two">
+				<div class="ljs-template-schedule__speaker-body">
+					<img
+						src="https://hvz5h5ar027h1qc4-zippykid.netdna-ssl.com/wp-content/uploads/2018/10/karlgroves.jpeg"
+						alt="Karl Groves">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">10:45 - 11:25</span>
+						<span class="ljs-template-schedule__name"><a href="hhttps://libertyjs.com/speakers/karl-groves/">Karl Groves</a></span>
+						<span class="ljs-template-schedule__title">Everything you need to know about JS accessibility</span>
+					</div>
+				</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-two">
+				<div class="ljs-template-schedule__speaker-body">
+					<img
+						src="https://hvz5h5ar027h1qc4-zippykid.netdna-ssl.com/wp-content/uploads/2018/10/AntonCaceres.jpeg"
+						alt="Anton Caceres">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">11:40 - 12:00</span>
+						<span class="ljs-template-schedule__name"><a href="https://libertyjs.com/speakers/anton-caceres/">Anton Caceres</a></span>
+						<span class="ljs-template-schedule__title">Mastering Web Performance Beyond Milliseconds</span>
+					</div>
+				</div>
+			</div>
+			<div class="ljs-template-schedule__divider">
+				<div>Lunch</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-one">
+				<div class="ljs-template-schedule__speaker-body">
+					<img
+						src="https://hvz5h5ar027h1qc4-zippykid.netdna-ssl.com/wp-content/uploads/2018/10/ChrisDeMars.jpg"
+						alt="Chris DeMars">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">01:30 - 03:05</span>
+						<span class="ljs-template-schedule__name"><a href="https://libertyjs.com/speakers/chris-demars/">Chris DeMars</a></span>
+						<span class="ljs-template-schedule__title">Rea11y Simple A11y: A Focused Accessibility Workshop </span>
+					</div>
+				</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-two">
+				<div class="ljs-template-schedule__speaker-body">
+					<img
+						src="https://hvz5h5ar027h1qc4-zippykid.netdna-ssl.com/wp-content/uploads/2018/10/vivian-guillen-e1538698600223.jpeg"
+						alt="Vivian Guillen">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">01:30 - 02:10</span>
+						<span class="ljs-template-schedule__name"><a href="https://libertyjs.com/speakers/vivan-guillen/">Vivian Guillen</a></span>
+						<span class="ljs-template-schedule__title">CSS for Backend Developers: How to Stop Fearing Your Stylesheets</span>
+					</div>
+				</div>
+
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-two">
+				<div class="ljs-template-schedule__speaker-body">
+					<img
+						src="https://hvz5h5ar027h1qc4-zippykid.netdna-ssl.com/wp-content/uploads/2018/10/samejones.png"
+						alt="Sam Jones">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">02:25 - 03:05</span>
+						<span class="ljs-template-schedule__name"><a href="https://libertyjs.com/speakers/sam-jones-2/">Sam Jones</a></span>
+						<span class="ljs-template-schedule__title">Trust Driven Development</span>
+					</div>
+				</div>
+
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-one">
+				<div class="ljs-template-schedule__speaker-body">
+					<img
+						src="https://hvz5h5ar027h1qc4-zippykid.netdna-ssl.com/wp-content/uploads/2018/10/yonatan.png"
+						alt="Yonatan Kra">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">03:20 - 4:55</span>
+						<span class="ljs-template-schedule__name"><a href="https://libertyjs.com/speakers/yonatan-kra/">Yonatan Kra</a></span>
+						<span class="ljs-template-schedule__title">Webpacker's guide to the galaxy</span>
+					</div>
+				</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-two">
+				<div class="ljs-template-schedule__speaker-body">
+					<img
+						src="https://hvz5h5ar027h1qc4-zippykid.netdna-ssl.com/wp-content/uploads/2018/10/KevinScott.jpg"
+						alt="Kevin Scott">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">03:20 - 04:00</span>
+						<span class="ljs-template-schedule__name"><a href="https://libertyjs.com/speakers/kevin-scott/">Kevin Scott</a></span>
+						<span class="ljs-template-schedule__title">Ask What AI Can Do for You</span>
+					</div>
+				</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-two">
+				<div class="ljs-template-schedule__speaker-body">
+					<img
+						src="https://hvz5h5ar027h1qc4-zippykid.netdna-ssl.com/wp-content/uploads/2018/10/AdamGiese.jpeg"
+						alt="Adam Giese">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">04:15 - 4:55</span>
+						<span class="ljs-template-schedule__name"><a href="https://libertyjs.com/speakers/adam-giese/">Adam Giese</a></span>
+						<span class="ljs-template-schedule__title">TComposing music with composed functions</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="ljs-template-schedule__container-day" id="ljs-template-schedule__day2">
+		<div class="ljs-template-schedule__date">
+			<p>NOV 2ND</p>
+			<p><a href="#ljs-template-schedule__day1">DAY 1</a> / <span class="ljs-template-schedule__date-active">DAY 2</span></p>
+		</div>
+		<div class="ljs-template-schedule__speakers">
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-three">
+				<div class="ljs-template-schedule__speaker-body">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">08:30 - 09:30</span>
+						<span class="ljs-template-schedule__name">Breakfast</span>
+						<span class="ljs-template-schedule__title"></span>
+					</div>
+				</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-three">
+				<div class="ljs-template-schedule__speaker-body">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">09:30 - 09:35</span>
+						<span class="ljs-template-schedule__name">Opening Remarks</span>
+						<span class="ljs-template-schedule__title"></span>
+					</div>
+				</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-one">
+				<div class="ljs-template-schedule__speaker-body">
+					<img
+						src="https://hvz5h5ar027h1qc4-zippykid.netdna-ssl.com/wp-content/uploads/2018/10/VueWorkshop-VictoriaAdams.jpg"
+						alt="Brian Gantick and Victoria Adams">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">09:40 - 11:15</span>
+						<span class="ljs-template-schedule__name"><a href="https://libertyjs.com/speakers/brian-gantick/">Brian Gantick</a> + <a href="https://libertyjs.com/speakers/victoria-adams/">Victoria Adams</a></span>
+						<span class="ljs-template-schedule__title">Creating Meaningful Application State: Animating with Vue.js </span>
+					</div>
+				</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-two">
+				<div class="ljs-template-schedule__speaker-body">
+					<img
+						src="https://libertyjs.com/wp-content/uploads/2017/11/conor-skiro-e1510843652769.png"
+						alt="Connor Skiro">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">09:40 - 10:20</span>
+						<span class="ljs-template-schedule__name"><a href="https://libertyjs.com/speakers/connor-skiro/">Connor Skiro</a></span>
+						<span class="ljs-template-schedule__title">Lean Coffee</span>
+					</div>
+				</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-two">
+				<div class="ljs-template-schedule__speaker-body">
+					<img
+						src="https://hvz5h5ar027h1qc4-zippykid.netdna-ssl.com/wp-content/uploads/2018/10/DaveRamirez.jpg"
+						alt="Dave Ramirez">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">10:35 - 11:15</span>
+						<span class="ljs-template-schedule__name"><a href="https://libertyjs.com/speakers/dave-ramirez/">Dave Ramirez</a></span>
+						<span class="ljs-template-schedule__title">Embracing the "Native" of React Native</span>
+					</div>
+				</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-two">
+				<div class="ljs-template-schedule__speaker-body">
+					<img
+						src="https://hvz5h5ar027h1qc4-zippykid.netdna-ssl.com/wp-content/uploads/2018/10/will-mitchell-e1538416593288.jpg"
+						alt="Will Mitchel">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">11:30 - 12:00</span>
+						<span class="ljs-template-schedule__name"><a href="https://libertyjs.com/speakers/will-mitchell/">Will Mitchel</a></span>
+						<span class="ljs-template-schedule__title">Front end testing that you wont hate</span>
+					</div>
+				</div>
+			</div>
+			<div class="ljs-template-schedule__divider">
+				<div>Lunch</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-one">
+				<div class="ljs-template-schedule__speaker-body">
+					<img
+						src="https://hvz5h5ar027h1qc4-zippykid.netdna-ssl.com/wp-content/uploads/2018/10/timellison.jpeg"
+						alt="Tim Ellison">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">01:30 - 03:05</span>
+						<span class="ljs-template-schedule__name"><a href="https://libertyjs.com/speakers/tim-ellison/">Tim Ellison</a></span>
+						<span class="ljs-template-schedule__title">Asynchronous JavaScript: From Callbacks to Async/Await</span>
+					</div>
+				</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-two">
+				<div class="ljs-template-schedule__speaker-body">
+					<img
+						src="https://hvz5h5ar027h1qc4-zippykid.netdna-ssl.com/wp-content/uploads/2018/10/jan.jpg"
+						alt="Jan Jorgesen">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">01:30 - 02:10</span>
+						<span class="ljs-template-schedule__name"><a href="https://libertyjs.com/speakers/jan-jorgensen/">Jan Jorgesen</a></span>
+						<span class="ljs-template-schedule__title">How to Learn Anything and Teach JavaScript</span>
+					</div>
+				</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-two">
+				<div class="ljs-template-schedule__speaker-body">
+					<img
+						src="https://hvz5h5ar027h1qc4-zippykid.netdna-ssl.com/wp-content/uploads/2018/10/SubyRaman.jpg"
+						alt="Suby Raman">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">02:25 - 03:05</span>
+						<span class="ljs-template-schedule__name"><a href="https://libertyjs.com/speakers/suby-raman/">Suby Raman</a></span>
+						<span class="ljs-template-schedule__title">The web Authenication API</span>
+					</div>
+				</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-one">
+				<div class="ljs-template-schedule__speaker-body">
+					<img
+						src="https://hvz5h5ar027h1qc4-zippykid.netdna-ssl.com/wp-content/uploads/2018/10/AngelRivera.gif"
+						alt="Angel Rivera">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">01:30 - 03:05</span>
+						<span class="ljs-template-schedule__name"><a href="https://libertyjs.com/speakers/angel-rivera/">Angel Rivera</a></span>
+						<span class="ljs-template-schedule__title">Asynchronous JavaScript: From Callbacks to Async/Await</span>
+					</div>
+				</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-two">
+				<div class="ljs-template-schedule__speaker-body">
+					<img
+						src="https://hvz5h5ar027h1qc4-zippykid.netdna-ssl.com/wp-content/uploads/2018/10/TkSmall.jpg"
+						alt="Taylor Krussen">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">03:20 - 04:00</span>
+						<span class="ljs-template-schedule__name"><a href="https://libertyjs.com/speakers/taylor-krusen/">Taylor Krussen</a></span>
+						<span class="ljs-template-schedule__title">What's all the fuss about serverless</span>
+					</div>
+				</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-two">
+				<div class="ljs-template-schedule__speaker-body">
+					<img
+						src="https://hvz5h5ar027h1qc4-zippykid.netdna-ssl.com/wp-content/uploads/2018/10/julka.jpeg"
+						alt="Julka Grodel">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">04:15 - 04:55</span>
+						<span class="ljs-template-schedule__name"><a href="https://libertyjs.com/speakers/julka-grodel/">Julka Grodel</a></span>
+						<span class="ljs-template-schedule__title">The Variable Crimes We Commit Against JavaScript</span>
+					</div>
+				</div>
+			</div>
+			<div class="ljs-template-schedule__speaker ljs-template-schedule__speaker-three">
+				<div class="ljs-template-schedule__speaker-body">
+					<div class="ljs-template-schedule__speaker-info">
+						<span class="ljs-template-schedule__time">TBA</span>
+						<span class="ljs-template-schedule__name">
+							Afterparty at TBA
+						</span>
+						<span class="ljs-template-schedule__title"></span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php
+
+get_footer();
